92c5b5801e0656a1041a6329d0b2d83d8ae89559
Mauricio da Rocha <mauriciodarocha@gmail.com>
2020-10-05 22:32:40 -0300

    +: CORS, SSL (Localhost). -+: Post request.

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 /**
- * Install express and run.
- * > npm start
+ * Install packages and run.
+ * $ npm start
+ * 
+ * Create keys:
+ * $ openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
  */
 
-const express = require('express')
-const app = express()
-const fs = require('fs')
-const port = 3000
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const keyPem = fs.readFileSync('./resources/key.pem');
+const certPem = fs.readFileSync('./resources/cert.pem');
+
+const app = express();
+const PORTSSL = 8443;
+
+const sslOptions = {
+    key: keyPem,
+    cert: certPem,
+    passphrase: "1234"
+};
 
 async function getJsonFile(path) {
     return new Promise((resolve, reject) => {
@@ -19,14 +35,18 @@ async function getJsonFile(path) {
     });
 }
 
-app.get('/api/:filename', (req, res, next) => {
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/api/:filename', (req, res) => {
     const filename = req.params.filename;
     getJsonFile(filename).then((data) => {
         const json = JSON.parse(data)
         res.json(json);
     }).catch((error) => {
         res.json({
-            "message": "File: "+filename+" not found."
+            "message": `File: ${filename} not found.`
         })
     })
 });
@@ -38,6 +58,5 @@ app.all('*', (req, res) => {
     res.status(200).send(json);
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+console.log(`Open: https://localhost:${PORTSSL}`);
+https.createServer(sslOptions, app).listen(PORTSSL);
